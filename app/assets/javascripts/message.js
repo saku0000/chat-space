0a6f831c25fc45c6e4f33d277026e4765b33a261
sakutetu <st581200@gmail.com>
2020-03-25 17:10:42 +0900

    jidou

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,45 +1,64 @@
 $(function(){
-  function buildHTML(message){
+  
+  
+    var buildHTML = function(message) {
+    if (message.content && message.image ) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+       
+        `<div class="upper-message">` +
+          `<div class="upper-message__user-name">` +
+            message.user_name +
+          `</div>` +
+           `<div class="upper-message__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<p class="lower-message__content">` +
+            message.content +
+          `</p>` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+      
+    } else if (message.content) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+       
+        `<div class="upper-message">` +
+          `<div class="upper-message__user-name">`+
+            message.user_name +
+          `</div>` +
+          `<div class="upper-message__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<p class="lower-message__content">` +
+            message.content +
+          `</p>` +
+        `</div>` +
+      `</div>`
+    } else if (message.image) {
+      
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="upper-message">` +
+        `<div class="upper-message__user-name">` +
+          message.user_name +
+        `</div>` +
+        `<div class="upper-message__date">` +
+          message.created_at +
+        `</div>` +
+      `</div>` +
+      `<div class="lower-message">` +
+        `<img src="` + message.image + `" class="lower-message__image" >` +
+      `</div>` +
+    `</div>`
     
-    if ( message.image ) {
-      var html =
-       `<div class="message">
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-          <img src=${message.image} >
-        </div>`
-      return html;
-    } else {
-      var html =
-       `<div class="message">
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-      return html;
     };
-  }
+    return html;
+  };
+
+  
   $('#new_message').on('submit', function(e){
   
     e.preventDefault();
@@ -58,7 +77,7 @@ $(function(){
       
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('.chat-main__list').animate({ scrollTop: $('.chat-main__list')[0].scrollHeight});
       $('form')[0].reset();
       $('.form__submit').removeAttr('disabled')
      
@@ -66,6 +85,43 @@ $(function(){
     .fail(function() {
       alert("メッセージ送信に失敗しました");
       $('.form__submit').removeAttr('disabled')
+
     });
   });
+
+
+  var reloadMessages = function() {
+    
+    var last_message_id = $('.message:last').data("message-id");
+  
+    $.ajax({
+      
+      url: "api/messages",
+      
+      type: 'get',
+      dataType: 'json',
+      
+      data: {id: last_message_id}
+    })
+
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        
+        var insertHTML = '';
+        
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        
+        $('.messages').append(insertHTML);
+        $('.chat-main__list').animate({ scrollTop: $('.chat-main__list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    })
+  }
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
